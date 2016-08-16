9b95f06ad6428d88b044f2a1f16fbc9f79988a75
Jason Warholic <warholic.jason@gmail.com>
2016-08-15 22:06:22 -0700

    update puppies.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+#SPECIES ------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Eyelash length: varies
+#Friendly: varies
+#Eye count: 2
+#Name: varies
+#Color: varies
+#legs: 4
+#tail: 1
+#size: varies
+
+
+#BEHAVIOR -----------------------
+#Jump
+#Hug
+#Scamper
+#Playful
+#Licks
+#Barks
