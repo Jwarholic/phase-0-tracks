73fcf3302e7728b0f8f7089618302eeb6ce8e4fd
Jason Warholic <warholic.jason@gmail.com>
2016-08-17 18:44:02 -0700

    update solo.rb release 0

---
+++ ruby/solo.rb
@@ -0,0 +1,20 @@
+#Release 0: Design a Class
+
+# -Create a class that creates a gym
+#   -Create a initialize method with @gym_name , @gym_type as parameters
+#   	-State our attributes
+#   	-give our class these atrributes
+# 	    -@gym_location
+#       -@gym_ranks as an array with the top gyms
+#   -Create a method that gives a high-five with n as a parameter
+#		-DEFINE high_five 
+#		- n.times puts High-Five!
+#   -Create a method that allows user to get protein with n as a parameter
+#		-DEFINE get_protein
+#		-puts out "Here is your #{n} grams of protein.
+#	-Create a method that deletes a gym from @gym_ranks
+#     -DEFINE bad_gym with gym as a parameter
+#     - @gym_type.delete(gym)
+#	-Create a method that adds a gym to @gym_ranks
+#     -DEFINE good_gym(gym)
+#     -@gym_type.add(gym)
