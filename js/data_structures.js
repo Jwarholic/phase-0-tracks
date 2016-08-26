1c35884fc540c2d95cd2e7f497f4800d7f3eeb12
Jason Warholic <warholic.jason@gmail.com>
2016-08-26 12:33:39 -0700

    add code to release1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,4 @@
+// Release 0:
 // Creat two arrays one array for colors and the other array for hourse's names
 // and print out the arrays
 var color = ["red", "blue" , "yellow", "green"]; 
@@ -13,3 +14,23 @@ color.push("gold");
 horse_names.push("nancy");
 console.log(color);
 console.log(horse_names);
+
+// Release: 1
+// -Take our two arrays color and horse_names (same length)
+//   -Make a  for loop to add all colors and horse_names to a new object
+//     -Create empty object
+//     -For each index of colors 
+//        -Add horse_names as a key in the new object
+//        -Add color as a value in the new object
+//   Convert to an object using horse names as keys
+//   colors as values
+
+ var horses = {};
+
+ for (var i = 0; i < color.length; i++) {
+  horses[horse_names[i]] = color[i];
+ }
+console.log(horses);
+
+
+
