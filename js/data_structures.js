7bee9f6a3fab84f6f7c3d2f2ae9a5f78d7f1c8bb
Jason Warholic <warholic.jason@gmail.com>
2016-08-26 11:35:31 -0700

    Finish release 0

---
+++ js/data_structures.js
@@ -0,0 +1,15 @@
+// Creat two arrays one array for colors and the other array for hourse's names
+// and print out the arrays
+var color = ["red", "blue" , "yellow", "green"]; 
+var horse_names =[ "ed", "joel", "jason", "betsy"]; 
+console.log(color);
+console.log(horse_names);
+
+
+// Add 1 new color to color array 
+// Add 1 new horse to the horse array
+
+color.push("gold");
+horse_names.push("nancy");
+console.log(color);
+console.log(horse_names);
