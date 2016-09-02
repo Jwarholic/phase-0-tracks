8e5f1b82ccb01c6fb384a1146b3da6d4a0208fae
Jason Warholic <warholic.jason@gmail.com>
2016-09-01 22:24:51 -0700

    Add pseudocode for sketch project

---
+++ databases/8.5project/gyms.rb
@@ -0,0 +1,29 @@
+# Assignment 8-5 Create something using Ruby and SQlite3
+
+
+# Input
+# Create a random workout generator that lets user pick from a database of workouts
+# User can choose to add or delete workouts from the database
+# User can choose a category to get a workout
+# User gets a random workout back at them
+# User can choose if they want a new one or choose to exit
+
+#Require our gems
+
+
+
+# Create default databases....
+#       - Arms
+#       - Legs
+#       - Chest
+#       -back
+#       - Shoulders
+#       - Abs
+#       - Full_body
+
+
+
+# Populate our databases with 3 workout names each ( can add more later) 
+
+
+# Add a user interface that will ask the user 
