9d759e4a1a8322d9a84b3eed836f8b1840534905
Jason Warholic <warholic.jason@gmail.com>
2016-09-03 19:42:28 -0700

    Update user interface so that all commands work properly

--- databases/8.5project/gyms.rb
+++ databases/8.5project/gyms.rb
@@ -83,6 +83,30 @@ def add(db, name, description, group)
 	db.execute("INSERT OR IGNORE INTO #{group} (name, description) VALUES (?, ?)", [name, description])
 end
 
+# Create a method that will allow user to delete workouts by the workout name
+def delete(db, name, group)
+	db.execute("DELETE FROM #{group} WHERE name = (?)", [name])
+end
+
+#method to update the description in a table
+def update_desc(db, name, desc, group)
+	db.execute("UPDATE #{group} SET description = (?)  WHERE name = (?)", [desc], [name]) 
+end
+
+#method to update name in a table
+def update_name(db, new_name, old_name, group)
+	db.execute("UPDATE #{group} SET name = (?) WHERE name = (?)", [new_name], [old_name])
+end
+
+# Method to view a table
+def print_table(db, name)
+	results = db.execute("SELECT * FROM #{name}")
+	results.each do |workout|
+		puts "Workout Name: #{workout['name']}:"
+		puts  "Workout Description: #{workout['description']}"
+	end
+end
+
 # USER INTERFACE
 
 # User chooses to add or delete workouts from the tables OR chooses to generate a workout
@@ -95,39 +119,86 @@ end
  puts "-- Type 'delete' to delete a workout"
  puts "-- Type 'update' to update a workout."
  puts "-- Type 'workout' to generate a random workout."
+ puts "-- Type 'print' to view a table."
  first_response = gets.chomp.downcase 
 
  #Set up a case statement for each response from the user
- case first_response
- 	when 'add'
-	puts "choose a database to add to (arms, legs, chest, shoulders, back)"
-	add = gets.chomp.downcase
-	if add == 'arms' || workout == 'legs' || workout == 'chest' || workout == 'back' || workout == 'shoulders'
-	puts "Type a name for the workout"
-	name = gets.chomp
-	puts "Type the description for the workout"
-	description = gets.chomp 
-	add(db, name, description, add)
+case first_response
+   when 'add'
+		puts "choose a group to add a workout (arms, legs, chest, shoulders, back)"
+		add = gets.chomp.downcase
+	if add == 'arms' || add == 'legs' || add == 'chest' || add == 'back' || add == 'shoulders'
+		puts "Type a name for the workout"
+		name = gets.chomp
+		puts "Type the description for the workout"
+		description = gets.chomp 
+		add(db, name, description, add)
+		puts "Added: #{name}: #{description}"
+	else 
+		puts "I didn't understand you."
 	end
-end
-	
+  when 'delete'
+		puts "choose a database to delete from (arms, legs, chest, shoulders, back)"
+		delete = gets.chomp.downcase
+	if delete == 'arms' || delete == 'legs' || delete == 'chest' || delete == 'back' || delete == 'shoulders'
+		puts "Type the name of the workout to remove"
+		name = gets.chomp
+		delete(db, name, delete)
+		puts "deleted: #{delete} (if it existed)"
+	else
+		puts "I didn't understand you."
+	end
+  when 'update'
+  	puts "choose a database to update from. (arms, legs, chest, shoulders, back)"
+  	update = gets.chomp.downcase
+  	if update == 'arms' || update == 'legs' || update == 'chest' || update == 'back' || update == 'shoulders'
+  		puts "Type 'name' to update a workout name or type 'description' to update a workout description."
+  		update_type = gets.chomp.downcase
+  		if update_type == 'name'
+  			puts "What workout name would you like to update?"
+  			name = gets.chomp
+  			puts "What do you want the new name to be?"
+  			new_name = gets.chomp
+  			update_name(db, new_name, name, update)
+  			puts "Changed #{name} to #{new_name}. (if it existed)"
+  		 elsif update_type == 'description'
+  		 	puts "What workout name would you like to update?"
+  		 	name = gets.chomp
+  		 	puts "What do you want the new description to be?"
+  		 	description = gets.chomp
+  		 	update_desc(db, name , description , update)
+  		 	puts "Updated: #{name} to #{description}. (if it existed)"
+  		 else puts "I didn't understand you."
+  		 end
+  	else puts "I didn't understand you."
+  	end
+	when 'workout'	
 #Ask user what workout to fetch from and converts it to an array
-puts "What muscle group are you working out today? (arms, legs, chest, back, shoulders)?"
-workout = gets.chomp.downcase
-
+	puts "What muscle group are you working out today? (arms, legs, chest, back, shoulders)?"
+	workout = gets.chomp.downcase
 #Iterate through the array and generate a random workout based on the input
-if workout == 'arms' || workout == 'legs' || workout == 'chest' || workout == 'back' || workout == 'shoulders'
-	workout = workout.split(' ')
-
-workout.each do |index|
-	index = db.execute("SELECT #{index}.name, #{index}.description FROM #{index}")
-	randnum = rand(0..index.length - 1)
-	randompick = index[randnum]
-    puts "Here is your random workout"
-	puts "----------------------"
-	puts "Workout Name: #{randompick['name']}"
-	puts "Workout Description: #{randompick['description']}"
-	end	
+		if workout == 'arms' || workout == 'legs' || workout == 'chest' || workout == 'back' || workout == 'shoulders'
+		workout = workout.split(' ')
+		workout.each do |index|
+		index = db.execute("SELECT #{index}.name, #{index}.description FROM #{index}")
+		randnum = rand(0..index.length - 1)
+		randompick = index[randnum]
+   	    puts "Here is your random workout"
+	    puts "----------------------"
+	    puts "Workout Name: #{randompick['name']}"
+	    puts "Workout Description: #{randompick['description']}"
+	    end	
+	    else
+	    puts "I didn't understand you. Please retry the program."
+ 	    end
+ 	when 'print'
+ 		puts "What table would you like to view? (arms, legs, chest, back, shoulders)?"
+ 		view = gets.chomp.downcase
+ 		if view == 'arms' || view == 'legs' || view == 'chest' || view == 'back' || view == 'shoulders'
+ 			print_table(db, view)
+ 		else
+ 			puts "not a valid table."
+ 		end
 else
-	puts "I didn't understand you"
+puts "I didn't understand you. Please retry the program." 
 end
