d460540e56110d29c22b3e58e51f9908070e98eb
Jason Warholic <warholic.jason@gmail.com>
2016-09-06 18:40:08 -0700

    Add release1 change background of body

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,12 @@
+console.log("The script is running!");
+
+
+
+// Change the background color of body to a new color
+var body = document.getElementsByTagName("body");
+mainbody = body[0];
+mainbody.style.backgroundColor = "grey";
+
+//Add a event listener
+// Make the lizard GIANT button make the lizzard bigger.
+// The tiny button to make him smaller.
